79261df1904cdceb5d7c8cc243d08945e6350046
sangbooom <tkdqja4164@naver.com>
2024-10-29 22:17:15 +0900

    feat: replace from useSyncExternalStore hook to use-sync-external-store/shim for react version

--- src/lib/context/useSyncModalStore.ts
+++ src/lib/context/useSyncModalStore.ts
@@ -1,4 +1,4 @@
-import { useSyncExternalStore } from "react";
+import { useSyncExternalStore } from "use-sync-external-store/shim";
 import { registerModalStore } from "./store";
 
 export const useSyncModalStore = () => {
